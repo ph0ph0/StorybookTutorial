c690dde4e7e9299e0e08b5472257399820eae892
ph0ph0 <phillipalanlee@googlemail.com>
2019-10-05 15:41:40 +0100

    creating screen

---
+++ src/components/InboxScreen.stories.js
@@ -0,0 +1,9 @@
+import React from 'react'
+import { storiesOf } from '@storybook/react'
+
+import { PureInboxScreen } from './InboxScreen'
+
+storiesOf("InboxScreen", module)
+    .add("default", () => <PureInboxScreen />)
+    .add("error", () => <PureInboxScreen error = "Something"/>)
+
